8fafeb1668e1be6e832163f9b3ef400bb6768560
Vuk Djukic <vuchina@icloud.com>
2019-06-06 14:48:17 -0700

    Update CustomSysErrMController.js

--- src/cmp/CustomSystemErrorMessage/CustomSysErrMController.js
+++ src/cmp/CustomSystemErrorMessage/CustomSysErrMController.js
@@ -1,20 +1,16 @@
 action.setCallback(this, function(resp) {
-if (resp.getState() === “SUCCESS”) {
-
-     component.set(“v.messageType”, ‘success’ );
-component.set(“v.message”, ‘Completed Successfully!’ );
-}
-else if (resp.getState() === “ERROR”) {
-
-     component.set(“v.messageType”, ‘error’ );
-var errors = resp.getError();
-if (errors) {
-if (errors[0] && errors[0].message) {
-component.set(“v.message”,errors[0].message );//Fetching Custom Message.
-        }
-}
-else {
-component.set(“v.message”, ‘Request Failed!’ );
-}
-}
+     if (resp.getState() === “SUCCESS”) {
+          component.set(“v.messageType”, ‘success’ );
+          component.set(“v.message”, ‘Completed Successfully!’ );
+     } else if (resp.getState() === “ERROR”) {
+          component.set(“v.messageType”, ‘error’ );
+          var errors = resp.getError();
+          if (errors) {
+               if (errors[0] && errors[0].message) {
+                    component.set(“v.message”,errors[0].message );//Fetching Custom Message.
+               }
+          } else {
+               component.set(“v.message”, ‘Request Failed!’ );
+               }
+          }
 });
